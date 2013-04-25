fb25421fea7a1f995635b8613c52863b2361c12e
Florent Benoit <Florent.Benoit@peergreen.com>
2013-04-25 17:53:28 +0200

    First import

---
+++ src/main/java/com/peergreen/webcontainer/base/lifecycle/WebApplicationLifeCyclePhaseProvider.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2012 Peergreen S.A.S.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.peergreen.webcontainer.base.lifecycle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.peergreen.deployment.DeploymentMode;
+import com.peergreen.deployment.FacetLifeCyclePhaseProvider;
+import com.peergreen.webcontainer.WebApplication;
+
+public class WebApplicationLifeCyclePhaseProvider implements FacetLifeCyclePhaseProvider<WebApplication> {
+
+    private final List<String> deployPhases;
+    private final List<String> updatePhases;
+    private final List<String> undeployPhases;
+
+    public WebApplicationLifeCyclePhaseProvider() {
+        this.deployPhases = new ArrayList<String>();
+        deployPhases.add("PARSE_XML");
+        deployPhases.add("DEPLOY");
+        deployPhases.add("START");
+
+        this.updatePhases = new ArrayList<String>();
+        deployPhases.add("STOP");
+        deployPhases.add("PARSE_XML");
+        deployPhases.add("START");
+
+
+        this.undeployPhases = new ArrayList<String>();
+        undeployPhases.add("STOP");
+        undeployPhases.add("UNDEPLOY");
+    }
+
+    @Override
+    public List<String> getLifeCyclePhases(DeploymentMode deploymentMode) {
+        switch (deploymentMode) {
+            case DEPLOY:
+                return deployPhases;
+            case UPDATE:
+                return updatePhases;
+            case UNDEPLOY:
+                return undeployPhases;
+                default : throw new IllegalStateException("Deployment mode '" + deploymentMode + "' not supported");
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/peergreen/webcontainer/base/processor/WARScannerProcessor.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2012 Peergreen S.A.S.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.peergreen.webcontainer.base.processor;
+
+import java.net.URI;
+
+import com.peergreen.deployment.Processor;
+import com.peergreen.deployment.ProcessorContext;
+import com.peergreen.deployment.ProcessorException;
+import com.peergreen.deployment.facet.archive.Archive;
+import com.peergreen.deployment.facet.archive.ArchiveException;
+import com.peergreen.webcontainer.WebApplication;
+
+/**
+ * WAR scanner.
+ * @author Florent Benoit
+ */
+public class WARScannerProcessor implements Processor<Archive> {
+
+
+    public WARScannerProcessor() {
+    }
+
+
+    /**
+     * If the archive is a war file then we will parse the web.xml and flag the archive as being a Web Application
+     */
+    @Override
+    public void handle(Archive archive, ProcessorContext processorContext) throws ProcessorException {
+
+        DefaultWebApplication webApplication = new DefaultWebApplication();
+
+        // Sets the URI of the web application
+        URI uri;
+        try {
+            uri = archive.getURI();
+        } catch (ArchiveException e) {
+            throw new ProcessorException("Unable to get URI of the archive", e);
+        }
+        webApplication.setURI(uri);
+
+        // Get context of the given Web application
+        String contextPath = uri.getPath();
+        if (contextPath.endsWith("/")) {
+            contextPath = contextPath.substring(0, contextPath.length() - 1);
+        }
+
+        // Keep only the name of the file without any extension
+        int lastSlash = contextPath.lastIndexOf("/");
+        if (lastSlash != -1) {
+            contextPath = contextPath.substring(lastSlash + 1, contextPath.length());
+        }
+        // remove the .extension
+        int dot = contextPath.lastIndexOf(".");
+        if (dot != -1) {
+            contextPath = contextPath.substring(0, dot);
+        }
+        contextPath = "/".concat(contextPath);
+        webApplication.setContextPath(contextPath);
+
+
+        // Add facet
+        processorContext.addFacet(WebApplication.class, webApplication);
+    }
+
+
+}
